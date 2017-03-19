55bab06f881260f3468e3e4a59dcac0367ce0589
Marcin Lewandowski <marcin@radiokit.org>
2017-03-19 19:29:37 +0100

    Added pull mode

--- lib/membrane_element_httpoison/source.ex
+++ lib/membrane_element_httpoison/source.ex
@@ -12,7 +12,7 @@ defmodule Membrane.Element.HTTPoison.Source do
   # Private API
 
   @doc false
-  def handle_init(%Options{method: method, location: location, headers: headers, body: body, options: options}) do
+  def handle_init(%Options{method: method, location: location, headers: headers, body: body, options: options, mode: mode}) do
     {:ok, %{
       location: location,
       method: method,
@@ -20,19 +20,44 @@ defmodule Membrane.Element.HTTPoison.Source do
       body: body,
       options: options,
       ref: nil,
+      mode: mode,
     }}
   end
 
 
   @doc false
-  def handle_play(%{method: method, location: location, body: body, headers: headers, options: options} = state) do
-    case HTTPoison.request(method, location, body, headers, options ++ [stream_to: self()]) do
+  def handle_play(%{method: method, location: location, body: body, headers: headers, options: options, mode: mode} = state) do
+    options = case mode do
+      :pull ->
+        options ++ [stream_to: self(), async: :once]
+     
+      :push ->
+        options ++ [stream_to: self()]
+    end
+
+    case HTTPoison.request(method, location, body, headers, options) do
       {:ok, %HTTPoison.AsyncResponse{id: ref}} ->
         {:ok, %{state | ref: ref}}
 
       {:error, reason} ->
         {:error, {:httperror, reason}}
     end
+  end
+
+
+  @doc false
+  def handle_event(:source, _caps, %Membrane.Event{type: :underrun}, %{ref: ref, mode: :pull} = state) do
+    case HTTPoison.stream_next(ref) do
+      {:ok, _response} ->
+        {:ok, state}
+
+      {:error, reason} ->
+        {:error, {:pull, reason}}
+    end
+  end
+
+  def handle_event(:source, _caps, %Membrane.Event{type: :underrun}, state) do
+    {:ok, state}
   end
 
 
--- lib/membrane_element_httpoison/source_options.ex
+++ lib/membrane_element_httpoison/source_options.ex
@@ -13,16 +13,21 @@ defmodule Membrane.Element.HTTPoison.Source.Options do
   * headers - request headers, in the same form as passed to `HTTPoison.request/5`,
     empty list by default,
   * options - request options, in the same form as passed to `HTTPoison.request/5`,
-    empty list by default. Please do not use `:stream_to` option here.
+    empty list by default. Please do not use `:stream_to` option here,
+  * mode - mode of operation, if it is `:push` it will generate buffers as fast
+    as they are being received from the network (this is the default). If it is 
+    `:pull` it will generate buffers if it receives Underrun Event on the `:source`
+    pad.
   """
 
-  defstruct location: nil, method: :get, body: "", headers: [], options: []
+  defstruct location: nil, method: :get, body: "", headers: [], options: [], mode: :push
 
   @type location_t :: String.t
   @type method_t :: :get | :post | :put | :patch | :delete | :head | :options
   @type body_t :: HTTPoison.body
   @type headers_t :: HTTPoison.headers
   @type options_t :: Keyword.t
+  @type mode_t :: :pull | :push
 
   @type t :: %Membrane.Element.HTTPoison.Source.Options{
     location: location_t,
@@ -30,5 +35,6 @@ defmodule Membrane.Element.HTTPoison.Source.Options do
     body: body_t,
     headers: headers_t,
     options: options_t,
+    mode: mode_t,
   }
 end
