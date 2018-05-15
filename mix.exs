8c1506eeb0a5f2fb463970611aed02a1da6c8c6e
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-05-15 12:10:17 +0200

    New .gitignore, LICENSE and mix.exs

--- mix.exs
+++ mix.exs
@@ -2,38 +2,41 @@ defmodule Membrane.Element.HTTPoison.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :membrane_element_httpoison,
-     compilers: Mix.compilers,
-     version: "0.0.1",
-     elixir: "~> 1.3",
-     elixirc_paths: elixirc_paths(Mix.env),
-     description: "Membrane Multimedia Framework (HTTPoison Element)",
-     maintainers: ["Marcin Lewandowski", "Mateusz Front"],
-     licenses: ["MIT"],
-     name: "Membrane Element: HTTPoison",
-     source_url: "http://github.com/membraneframework/membrane-element-httpoison",
-     preferred_cli_env: [espec: :test],
-     deps: deps()]
+    [
+      app: :membrane_element_httpoison,
+      compilers: Mix.compilers(),
+      version: "0.0.1",
+      elixir: "~> 1.6",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description: "Membrane Multimedia Framework (HTTPoison Element)",
+      package: package(),
+      name: "Membrane Element: HTTPoison",
+      source_url: "http://github.com/membraneframework/membrane-element-httpoison",
+      deps: deps()
+    ]
   end
-
 
   def application do
-    [applications: [
-      :membrane_core,
-      :httpoison
-    ], mod: {Membrane.Element.HTTPoison, []}]
+    [
+      extra_applications: [],
+      mod: {Membrane.Element.HTTPoison, []}
+    ]
   end
 
-
   defp elixirc_paths(:test), do: ["lib", "spec/support"]
-  defp elixirc_paths(_),     do: ["lib",]
-
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
       {:membrane_core, git: "git@github.com:membraneframework/membrane-core.git"},
-      # {:membrane_core, path: "/Users/marcin/aktivitis/radiokit/membrane-core"},
-      {:httpoison, "~> 1.0.0"},
+      {:httpoison, "~> 1.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Membrane Team"],
+      licenses: ["Apache 2.0"]
     ]
   end
 end
