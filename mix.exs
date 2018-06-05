5493eebd0c34e75525ea37d8cd34855a276ae571
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-06-05 10:11:57 +0200

    Pre-release refinement

--- mix.exs
+++ mix.exs
@@ -5,13 +5,14 @@ defmodule Membrane.Element.HTTPoison.Mixfile do
     [
       app: :membrane_element_httpoison,
       compilers: Mix.compilers(),
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       description: "Membrane Multimedia Framework (HTTPoison Element)",
       package: package(),
       name: "Membrane Element: HTTPoison",
-      source_url: "http://github.com/membraneframework/membrane-element-httpoison",
+      source_url: link(),
+      homepage_url: "https://membraneframework.org",
       deps: deps()
     ]
   end
@@ -26,18 +27,24 @@ defmodule Membrane.Element.HTTPoison.Mixfile do
   defp elixirc_paths(:test), do: ["lib", "spec/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  defp deps do
-    [
-      {:membrane_core, git: "git@github.com:membraneframework/membrane-core.git"},
-      {:mockery, "~> 2.1", runtime: false},
-      {:httpoison, "~> 1.1.0"}
-    ]
+  defp link do
+    "http://github.com/membraneframework/membrane-element-httpoison"
   end
 
   defp package do
     [
       maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"]
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => link()}
+    ]
+  end
+
+  defp deps do
+    [
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
+      {:membrane_core, git: "git@github.com:membraneframework/membrane-core.git"},
+      {:mockery, "~> 2.1", runtime: false},
+      {:httpoison, "~> 1.1.0"}
     ]
   end
 end
